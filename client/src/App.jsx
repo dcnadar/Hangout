4db3dbc22d360fdc57ab2370502eb1bcc6de6c2f
justagit07 <droptelegram@outlook.com>
2024-03-12 17:28:22 +0530

    create the frontend with the react with vite and download all the dependency that are required.

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='border-2 border-solid  border-red-600'>hnji kya hal h 
+
+    </div>
+
+    </>
+  )
+}
+
+export default App
+// border-2 border-solid  border-red-600
